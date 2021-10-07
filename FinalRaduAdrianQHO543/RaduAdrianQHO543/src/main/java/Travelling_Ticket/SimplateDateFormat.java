72c62a61e037f34bf530347fd2199a9c2c567619
radducuu <76048004+radducuu@users.noreply.github.com>
2021-10-07 23:07:23 +0100

    Add files via upload

---
+++ FinalRaduAdrianQHO543/RaduAdrianQHO543/src/main/java/Travelling_Ticket/SimplateDateFormat.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Travelling_Ticket;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Radu Adrian
+ */
+public class SimplateDateFormat {
+
+    SimplateDateFormat(String ddMMyyyy_) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String format(Date time) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
